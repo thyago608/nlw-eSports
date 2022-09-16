23dde6181d794c686ec599138767a55f2dfff7ae
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-09-16 19:38:30 -0400

    Created Modal Frontend

---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,20 @@
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+}
+
+export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
+  return (
+    <a href="#" className="relative rounded-lg overflow-hidden">
+      <img src={bannerUrl} alt={title} />
+
+      <div className="absolute bottom-0 left-0 right-0 w-full pt-16 pb-4 px-4 bg-game-gradient">
+        <strong className="font-bold text-white block">{title}</strong>
+        <span className="text-zinc-300 text-sm block">
+          {adsCount} anúncio(s)
+        </span>
+      </div>
+    </a>
+  );
+}
